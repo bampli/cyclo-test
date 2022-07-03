57871c925c3b59024cb0cd9cdb9ada2e0161b4ec
Jose Motta <josemotta@bampli.com>
2022-07-03 14:35:47 -0300

    zoom tests

--- src/src/App.tsx
+++ src/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import { SunburstChart } from './SunburstChart';
+//import { ZoomableSunburst } from './ZoomableSunburst';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        {/* <ZoomableSunburst/> */}
         <SunburstChart/>
       </header>
     </div>
